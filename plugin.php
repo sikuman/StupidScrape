7c26f2f206776b7d470ae13b98ee2ec399be02da
sikuman <31029528+sikuman@users.noreply.github.com>
2017-08-15 14:48:07 +0700

    add file
     add initial file

---
+++ plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Google Image for StupidPie
+Plugin URI:  http://www.dojo.cc
+Description: Penambahan kemampuan memakai Google Image untuk StupidPie
+Version:     0.1
+Author:      Internet Marketing Dojo
+Author URI:  http://www.dojo.cc
+License:     Halal
+*/
+
+use Buchin\GoogleImageGrabber\GoogleImageGrabber;
+
+define('SPP_GOOGLE_IMAGE', dirname(__FILE__));
+
+
+require_once(SPP_GOOGLE_IMAGE . '/vendor/autoload.php');
+
+function scrape_google_image($keyword)
+{
+	$images = GoogleImageGrabber::grab($keyword);
+
+	return collect($images);
+}
