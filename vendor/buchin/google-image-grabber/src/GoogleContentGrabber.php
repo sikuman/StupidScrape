fb8ba08e226b7bbac98a226a85e92ecfb23d2575
sikuman <31029528+sikuman@users.noreply.github.com>
2017-08-15 15:29:48 +0700

    +change funcion

--- vendor/buchin/google-image-grabber/src/GoogleContentGrabber.php
+++ vendor/buchin/google-image-grabber/src/GoogleContentGrabber.php
@@ -11,6 +11,7 @@ class GoogleContentGrabber
 	public static function grab($keyword, $options = [])
 	{
 		$url = "https://www.google.com/search?q=" . urlencode($keyword) . "&source=lnms&tbm=isch&tbs=";
+		$url = 'http://www.google.com/search?hl=en&q='.$keyword;
 
 		$ua = \Campo\UserAgent::random([
 		    'os_type' => ['Windows', 'OS X'],
@@ -34,27 +35,40 @@ class GoogleContentGrabber
 
 
 		$htmldom = new Dom;
-		$htmldom->loadStr($response, []);
+		// $htmldom->loadStr($response, []);
+		$htmldom->loadStr($response);
+		foreach($htmldom->find('div.g') as $g)
+			{
+				/*
+				 * each search results are in a list item with a class name 'g'
+				 * we are seperating each of the elements within, into an array
+				 * Summaries are stored in a span with a classname of 'st'
+				 * Title are stored in a h3 with a classname of 'r'
+				 */
+				$item['title'] = isset($g->find('h3.r', 0)->innertext) ? $g->find('h3.r', 0)->innertext : '';
+				$item['description'] = isset($g->find('span.st', 0)->innertext) ? $g->find('span.st', 0)->innertext : '';
+				$result[] =  $item;
+			}
 
-		$results = [];
+		// $results = [];
 		
-		foreach ($htmldom->find('.rg_di > .rg_meta') as $n => $dataset) {
+		// foreach ($htmldom->find('.rg_di > .rg_meta') as $n => $dataset) {
 
-			$jsondata = $dataset->text;
-			$data = json_decode($jsondata);
+			// $jsondata = $dataset->text;
+			// $data = json_decode($jsondata);
 
-		    $results[$n]['keyword'] = $keyword;
-		    $results[$n]['slug'] = __::slug($keyword);
+		    // $results[$n]['keyword'] = $keyword;
+		    // $results[$n]['slug'] = __::slug($keyword);
 
-		    $results[$n]['title'] = ucwords(__::slug($data->pt, ['delimiter' => ' ']));
-		    $results[$n]['alt'] = __::slug($data->s, ['delimiter' => ' ']);
+		    // $results[$n]['title'] = ucwords(__::slug($data->pt, ['delimiter' => ' ']));
+		    // $results[$n]['alt'] = __::slug($data->s, ['delimiter' => ' ']);
 		    
-		    $results[$n]['url'] = $data->ou;
-		    $results[$n]['filetype'] = $data->ity;
-		    $results[$n]['width'] = $data->ow;
-		    $results[$n]['height'] = $data->oh;
-		    $results[$n]['source'] = $data->ru;
-		}
+		    // $results[$n]['url'] = $data->ou;
+		    // $results[$n]['filetype'] = $data->ity;
+		    // $results[$n]['width'] = $data->ow;
+		    // $results[$n]['height'] = $data->oh;
+		    // $results[$n]['source'] = $data->ru;
+		// }
 
 		return $results;
 	}
